472872c5ca4481ff54593fecb8965e5e5d41f149
Nikita Mishutin <mishutin.ns@gmail.com>
2024-04-12 16:59:46 +0300

    Revert "add content component"
    This reverts commit 3ba13c58d101c1e47ba135420e68ee039f6905e8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Container } from "@pages/Container";
-import { Content } from "@pages/Content";
 import { Header } from "@pages/Header";
 import React from "react";
 
@@ -7,9 +6,8 @@ function App() {
   return (
     <Container>
       <Header />
-      <Content />
     </Container>
-  );
+  )
 }
 
 export default App;
