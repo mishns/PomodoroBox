8deb24fc39bf77bb1ce4971fdce1a6050e84b27a
Nikita Mishutin <mishutin.ns@gmail.com>
2024-04-12 20:39:26 +0300

    add home block page

--- src/pages/Content/Content.tsx
+++ src/pages/Content/Content.tsx
@@ -3,13 +3,12 @@ import styles from "./content.css";
 import { Instruction } from "@ui/Instruction";
 import { TaskForm } from "@pages/TaskForm";
 import { TaskList } from "@pages/TaskList";
+import { HomeBlock } from "@pages/HomeBlock";
 
 export const Content: FC = () => {
   return (
     <div className={styles.content}>
-      <Instruction />
-      <TaskForm />
-      <TaskList />
+      <HomeBlock />
     </div>
   );
 };
