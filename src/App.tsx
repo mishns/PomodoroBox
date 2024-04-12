5d0bdfb7545753395579e99e904dec9175a641ac
Nikita Mishutin <mishutin.ns@gmail.com>
2024-04-12 15:36:24 +0300

    add logo ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { Logo } from "@ui/Logo";
 import React from "react";
-import { Button } from "@ui/Button";
 
 function App() {
-  return <Button isNegative>Добавить</Button>;
+  return <Logo />;
 }
 
 export default App;
--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -1,8 +1,14 @@
 import { default as React, FC } from "react";
 import styles from "./logo.css";
+import img from "@assets/img/pomodorobox-logo.svg";
 
 interface LogoProps {}
 
 export const Logo: FC<LogoProps> = () => {
-  return <div className={styles.logo}></div>;
+  return (
+    <div className={styles.logo}>
+      <img className={styles.pomodoroImg} src={img} alt="pomodoro-logo" />
+      <span className={styles.pomodoroTitle}>pomodoro_box</span>
+    </div>
+  );
 };
