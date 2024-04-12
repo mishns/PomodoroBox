f74df9fbb75a2409d4ecb86d7cd0dcc147be265e
Nikita Mishutin <mishutin.ns@gmail.com>
2024-04-12 17:24:32 +0300

    fix content component

--- src/pages/Content/Content.tsx
+++ src/pages/Content/Content.tsx
@@ -1,13 +1,11 @@
 import { default as React, FC } from "react";
 import styles from "./content.css";
+import { Button } from "@ui/Button";
 
-
-interface ContentProps {
-  
-}
-
-export const Content:FC<ContentProps> = () => {
+export const Content: FC = () => {
   return (
-
+    <div className={styles.content}>
+      <Button>Удалить</Button>
+    </div>
   );
-}
+};
